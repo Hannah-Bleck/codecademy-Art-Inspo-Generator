0600e94f249300f19da038ccee59b5833361d659
Hannah-Bleck <127896891+Hannah-Bleck@users.noreply.github.com>
2023-10-22 15:29:31 +0100

    testing-first-commit-and-setup

---
+++ art-inspo-gen.js
@@ -0,0 +1,20 @@
+/* Art Inspiration Generator
+
+Aim: to randomly generate an art prompt of one thing (single or plural),
+one verb/ action and up to three despriptors.
+
+*/
+
+// hold collection of words randomly generated
+let inspoCollection = [];
+
+// hold randomly generated elements 
+let thing = "";
+let action = "";
+let descriptors = [];
+
+thing = "cat";
+
+console.log(thing);
+
+// testing initial commit
