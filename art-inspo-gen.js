b0b270502072bc2d87d2307c314a38d49c61c2ac
Hannah-Bleck <127896891+Hannah-Bleck@users.noreply.github.com>
2023-10-22 21:05:40 +0100

    message generator created

--- art-inspo-gen.js
+++ art-inspo-gen.js
@@ -5,16 +5,59 @@ one verb/ action and up to three despriptors.
 
 */
 
-// hold collection of words randomly generated
-let inspoCollection = [];
+// THINGS
+const thingsSingular = ["wizard", "toad", "tree", "potion", "castle", "book", "knight", "rose"];
+const thingsPlural = ["company", "herd", "flock", "bandits", "pirates", "troop", "team", "school"];
 
-// hold randomly generated elements 
-let thing = "";
-let action = "";
-let descriptors = [];
+// ACTION
+const actionOpt = ["fighting", "reading", "dancing", "brewing", "crafting", "flying", "running", "eating", "sleeping"];
 
-thing = "cat";
+// DESCRIPTIONS
+const descriptions = ["purple", "red", "blue", "yellow", "quiet", "mysterious", "happy", "grumpy", "bold", "inquisitive", "cloudy", "sunny"];
 
-console.log(thing);
 
-// testing initial commit
+// message object
+const message = (thingType, numberOfDescriptions) => {
+    return{
+        _thing : "",
+        _action: "",
+        _descriptors: [],
+        generateThing(thingType){
+                if(thingType === "s"){
+                    this._thing = thingsSingular[Math.floor(Math.random()*(thingsSingular.length+1))];
+                }else if(thingType === "p"){
+                    this._thing = thingsPlural[Math.floor(Math.random()*(thingsPlural.length+1))];
+                }else{
+                    console.log("invalid options selected for thing");
+                }
+            },
+        generateAction(){
+            this._action = actionOpt[Math.floor(Math.random()*(actionOpt.length+1))];
+        },
+        generateDescriptions(numberOfDescriptions){
+            if(numberOfDescriptions <= 3){
+                for(let i = 0; i <= numberOfDescriptions; i++){
+                    let tempDesc = descriptions[Math.floor(Math.random()*(descriptions.length+1))];
+                    this._descriptors.push(tempDesc);
+                    }
+                }
+            }
+        }
+
+    };
+
+
+// generate a new message
+const generateMessage = (thingType, numberOfDescriptions) => {
+    let newMessage = message();
+    newMessage.generateThing(thingType);
+    newMessage.generateAction();
+    newMessage.generateDescriptions(numberOfDescriptions);
+    return newMessage;
+}
+
+let messageTest = generateMessage("p", 2);
+
+console.log(generateMessage("s", 3));
+
+console.log(messageTest._descriptors);
